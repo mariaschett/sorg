06b92decb61bb449825c1bd03cef99ab9d73f91c
Maria A Schett <mail@maria-a-schett.net>
2019-06-14 11:31:35 +0100

    Use rule type in tests

--- test/test_generalize.ml
+++ test/test_generalize.ml
@@ -1,3 +1,4 @@
+open Rule
 open Generalize
 open Ebso
 open Instruction
@@ -9,71 +10,75 @@ let suite = "suite" >::: [
         let s = [DUP I] and t = [DUP I]
         in
         assert_equal
-          ~cmp:[%eq: (Program.t * Program.t) list]
-          ~printer:[%show: (Program.t * Program.t) list]
-          [(s, t)] (generalize s t)
+          ~cmp:[%eq: Rule.t list]
+          ~printer:[%show: Rule.t list]
+          [{lhs = s; rhs = t}] (generalize s t)
       );
 
     "Remove superflous suffix" >::(fun _ ->
         let s = [CALLVALUE; DUP I; ISZERO] in
         let t = [CALLVALUE; CALLVALUE; ISZERO] in
-        let l = [CALLVALUE; DUP I] and r = [CALLVALUE; CALLVALUE]
+        let r = {lhs = [CALLVALUE; DUP I];
+                 rhs = [CALLVALUE; CALLVALUE]}
         in
-        assert_equal ~cmp:[%eq: (Program.t * Program.t) list]
-          ~printer:[%show: (Program.t * Program.t) list]
-          [(l, r)] (generalize s t)
+        assert_equal ~cmp:[%eq: Rule.t list]
+          ~printer:[%show: Rule.t list]
+          [r] (generalize s t)
       );
 
     "Abstract PUSH argument" >::(fun _ ->
         let s = [PUSH (Val "2"); DUP II; SWAP I] and t = [DUP I; PUSH (Val "2")] in
-        let l = [PUSH (Const "c"); DUP II; SWAP I] and r = [DUP I; PUSH (Const "c")]
+        let r = { lhs = [PUSH (Const "c"); DUP II; SWAP I];
+                  rhs = [DUP I; PUSH (Const "c")]}
         in
         assert_equal
-          ~cmp:[%eq: (Program.t * Program.t) list]
-          ~printer:[%show: (Program.t * Program.t) list]
-          [(l, r)] (generalize s t)
+          ~cmp:[%eq: Rule.t list]
+          ~printer:[%show: Rule.t list]
+          [r] (generalize s t)
       );
 
     "Remove superflous prefix, abstract PUSH argument" >::(fun _ ->
         let s = [POP; PUSH (Val "3"); SWAP I; POP] in
         let t = [POP; POP; PUSH (Val "3")] in
-        let l = [PUSH (Const "c"); SWAP I; POP] and r = [POP; PUSH (Const "c")]
+        let r = { lhs = [PUSH (Const "c"); SWAP I; POP];
+                  rhs = [POP; PUSH (Const "c")]}
         in
-        assert_equal ~cmp:[%eq: (Program.t * Program.t) list]
-          ~printer:[%show: (Program.t * Program.t) list]
-          [(l, r)] (generalize s t)
+        assert_equal ~cmp:[%eq: Rule.t list]
+          ~printer:[%show: Rule.t list]
+          [r] (generalize s t)
       );
 
     "Abstract PUSH args, two rules" >::(fun _ ->
         let s = [PUSH (Val "0"); PUSH (Val "0"); ADD] in
         let t = [PUSH (Val "0")] in
-        let l1 = [PUSH (Val "0"); PUSH (Const "c"); ADD] in
-        let l2 = [PUSH (Const "c"); PUSH (Val "0"); ADD] in
-        let r = [PUSH (Const "c")]
+        let r1 = { lhs = [PUSH (Val "0"); PUSH (Const "c"); ADD];
+                   rhs = [PUSH (Const "c")]} in
+        let r2 = { lhs = [PUSH (Const "c"); PUSH (Val "0"); ADD];
+                   rhs = [PUSH (Const "c")]}
         in
-        assert_equal ~cmp:[%eq: (Program.t * Program.t) list]
-          ~printer:[%show: (Program.t * Program.t) list]
-          [(l1, r); (l2, r)] (generalize s t)
+        assert_equal ~cmp:[%eq: Rule.t list]
+          ~printer:[%show: Rule.t list]
+          [r1; r2] (generalize s t)
       );
 
     "Advanced constant folding">::(fun _ ->
         let s = [PUSH (Val "1"); PUSH (Val "2"); DUP II; OR] in
         let t = [PUSH (Val "1"); PUSH (Val "3")]
         in
-        assert_equal ~cmp:[%eq: (Program.t * Program.t) list]
-          ~printer:[%show: (Program.t * Program.t) list]
-          [(s, t)] (generalize s t)
+        assert_equal ~cmp:[%eq: Rule.t list]
+          ~printer:[%show: Rule.t list]
+          [{lhs = s; rhs = t}] (generalize s t)
       );
 
     "ADD commutative, combines remove pre-/suffix and generalize argument" >::(fun _ ->
         let s = [POP; PUSH (Val "3"); DUP II; ADD; SWAP I; POP; PUSH (Val "2")] in
         let t = [POP; PUSH (Val "3"); ADD; PUSH (Val "2")] in
-        let l = [PUSH (Const "c"); DUP II; ADD] in
-        let r = [PUSH (Const "c")]
+        let r = { lhs = [PUSH (Const "c"); DUP II; ADD];
+                  rhs = [PUSH (Const "c")] }
         in
-        assert_equal ~cmp:[%eq: (Program.t * Program.t) list]
-          ~printer:[%show: (Program.t * Program.t) list]
-          [(l, r)] (generalize s t)
+        assert_equal ~cmp:[%eq: Rule.t list]
+          ~printer:[%show: Rule.t list]
+          [r] (generalize s t)
       );
   ]
 
