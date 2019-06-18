015daf4b75068d33d79863cde3dd1af44aa7c26e
Maria A Schett <mail@maria-a-schett.net>
2019-06-18 09:32:52 +0100

    Restructure tests, add todos for failing tests

--- test/test_generalize.ml
+++ test/test_generalize.ml
@@ -6,20 +6,54 @@ open OUnit2
 
 let suite = "suite" >::: [
 
+    (* equiv *)
+
     "Two equal constant terms are equvivalent">::(fun _ ->
         let s = [DUP I] and t = [DUP I]
         in
         assert_bool "" (equiv s t)
       );
 
-    "Check optimization: remove superflous suffix" >::(fun _ ->
+    "Remove superflous suffix" >::(fun _ ->
         let s = [CALLVALUE; DUP I; ISZERO] in
         let t = [CALLVALUE; CALLVALUE; ISZERO]
         in
         assert_bool "" (equiv s t)
       );
 
+    "Abstract PUSH argument" >::(fun _ ->
+        let s = [PUSH (Val "2"); DUP II; SWAP I] and t = [DUP I; PUSH (Val "2")] in
+        assert_bool "" (equiv s t)
+      );
+
+    "Remove superflous prefix, abstract PUSH argument" >::(fun _ ->
+        let s = [POP; PUSH (Val "3"); SWAP I; POP] in
+        let t = [POP; POP; PUSH (Val "3")] in
+        assert_bool "" (equiv s t)
+      );
+
+    "Abstract PUSH args, two rules" >::(fun _ ->
+        let s = [PUSH (Val "0"); PUSH (Val "0"); ADD] in
+        let t = [PUSH (Val "0")] in
+        assert_bool "" (equiv s t)
+      );
+
+    "Advanced constant folding">::(fun _ ->
+        let s = [PUSH (Val "1"); PUSH (Val "2"); DUP II; OR] in
+        let t = [PUSH (Val "1"); PUSH (Val "3")] in
+        assert_bool "" (equiv s t)
+      );
+
+    "ADD commutative, combines [..]" >::(fun _ ->
+        let s = [POP; PUSH (Val "3"); DUP II; ADD; SWAP I; POP; PUSH (Val "2")] in
+        let t = [POP; PUSH (Val "3"); ADD; PUSH (Val "2")] in
+        assert_bool "" (equiv s t)
+      );
+
+    (* generalize *)
+
     "Remove superflous suffix" >::(fun _ ->
+        todo "generalize not implemented";
         let s = [CALLVALUE; DUP I; ISZERO] in
         let t = [CALLVALUE; CALLVALUE; ISZERO] in
         let r = {lhs = [CALLVALUE; DUP I];
@@ -30,12 +64,8 @@ let suite = "suite" >::: [
           [r] (generalize s t)
       );
 
-    "Check optimization: Abstract PUSH argumetn" >::(fun _ ->
-        let s = [PUSH (Val "2"); DUP II; SWAP I] and t = [DUP I; PUSH (Val "2")] in
-        assert_bool "" (equiv s t)
-      );
-
     "Abstract PUSH argument" >::(fun _ ->
+        todo "generalize not implemented";
         let s = [PUSH (Val "2"); DUP II; SWAP I] and t = [DUP I; PUSH (Val "2")] in
         let r = { lhs = [PUSH (Const "c"); DUP II; SWAP I];
                   rhs = [DUP I; PUSH (Const "c")]}
@@ -46,13 +76,8 @@ let suite = "suite" >::: [
           [r] (generalize s t)
       );
 
-    "Check optimization: Remove superflous prefix, abstract PUSH argument" >::(fun _ ->
-        let s = [POP; PUSH (Val "3"); SWAP I; POP] in
-        let t = [POP; POP; PUSH (Val "3")] in
-        assert_bool "" (equiv s t)
-      );
-
     "Remove superflous prefix, abstract PUSH argument" >::(fun _ ->
+        todo "generalize not implemented";
         let s = [POP; PUSH (Val "3"); SWAP I; POP] in
         let t = [POP; POP; PUSH (Val "3")] in
         let r = { lhs = [PUSH (Const "c"); SWAP I; POP];
@@ -64,6 +89,7 @@ let suite = "suite" >::: [
       );
 
     "Remove superflous prefix" >::(fun _ ->
+        todo "generalize not implemented";
         let s = [POP; PUSH (Val "0"); ADD] in
         let t = [POP] in
         let r = { lhs = [PUSH (Val "0"); ADD];
@@ -74,13 +100,8 @@ let suite = "suite" >::: [
           [r] (generalize s t)
       );
 
-    "Check optimization: Abstract PUSH args, two rules" >::(fun _ ->
-        let s = [PUSH (Val "0"); PUSH (Val "0"); ADD] in
-        let t = [PUSH (Val "0")] in
-        assert_bool "" (equiv s t)
-      );
-
     "Abstract PUSH args, two rules" >::(fun _ ->
+        todo "generalize not implemented";
         let s = [PUSH (Val "0"); PUSH (Val "0"); ADD] in
         let t = [PUSH (Val "0")] in
         let r1 = { lhs = [PUSH (Val "0"); PUSH (Const "c"); ADD];
@@ -93,13 +114,8 @@ let suite = "suite" >::: [
           [r1; r2] (generalize s t)
       );
 
-    "Check optimization: Advanced constant folding">::(fun _ ->
-        let s = [PUSH (Val "1"); PUSH (Val "2"); DUP II; OR] in
-        let t = [PUSH (Val "1"); PUSH (Val "3")] in
-        assert_bool "" (equiv s t)
-      );
-
     "Advanced constant folding">::(fun _ ->
+        todo "generalize not implemented";
         let s = [PUSH (Val "1"); PUSH (Val "2"); DUP II; OR] in
         let t = [PUSH (Val "1"); PUSH (Val "3")]
         in
@@ -108,13 +124,8 @@ let suite = "suite" >::: [
           [{lhs = s; rhs = t}] (generalize s t)
       );
 
-    "Check optimization: ADD commutative, combines [..]" >::(fun _ ->
-        let s = [POP; PUSH (Val "3"); DUP II; ADD; SWAP I; POP; PUSH (Val "2")] in
-        let t = [POP; PUSH (Val "3"); ADD; PUSH (Val "2")] in
-        assert_bool "" (equiv s t)
-      );
-
     "ADD commutative, combines remove pre-/suffix and generalize argument" >::(fun _ ->
+        todo "generalize not implemented";
         let s = [POP; PUSH (Val "3"); DUP II; ADD; SWAP I; POP; PUSH (Val "2")] in
         let t = [POP; PUSH (Val "3"); ADD; PUSH (Val "2")] in
         let r = { lhs = [PUSH (Const "c"); DUP II; ADD];
