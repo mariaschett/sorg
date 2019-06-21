800873d1e4685091ea6a1c60dc007f9e3de1de61
Maria A Schett <mail@maria-a-schett.net>
2019-06-21 11:51:43 +0100

    Introduce types for substitution

--- lib/subst.ml
+++ lib/subst.ml
@@ -2,29 +2,33 @@ open Core
 open Ebso
 open Stackarg
 
-type var_subst = (constarg * constarg) list [@@deriving show { with_path = false }, sexp]
+type vvar = Stackarg.t [@@deriving show { with_path = false }, sexp, compare, equal]
+type vval = Stackarg.t [@@deriving show { with_path = false }, sexp, compare, equal]
 
-let equal_var_subst s1 s2 = List.equal [%eq: constarg * constarg]
-    (List.sort ~compare:[%compare: constarg * constarg] s1)
-    (List.sort ~compare:[%compare: constarg * constarg] s2)
+type ventr = (vvar * vval) [@@deriving show { with_path = false }, sexp, compare, equal]
+type t = ventr list [@@deriving show { with_path = false }, sexp, compare, equal]
 
-let is_mapped a s = List.Assoc.mem s a ~equal:[%eq: constarg]
+let equal_var_subst s1 s2 = List.equal [%eq: ventr]
+    (List.sort ~compare:[%compare: ventr] s1)
+    (List.sort ~compare:[%compare: ventr] s2)
 
-let map_exn a s = List.Assoc.find_exn s a ~equal:[%eq: constarg]
+let is_mapped a s = List.Assoc.mem s a ~equal:[%eq: vvar]
+
+let map_exn a s = List.Assoc.find_exn s a ~equal:[%eq: vval]
 
 (* only extend if is_mapped a1 s is false *)
-let map_extend a1 a2 s = List.Assoc.add s a1 a2 ~equal:[%eq: constarg]
+let map_extend a1 a2 s = List.Assoc.add s a1 a2 ~equal:[%eq: vval]
 
-let update_var_subst a1 a2 s =
-  if not (is_mapped a1 s)
-  then Some (map_extend a1 a2 s)
-  else if (map_exn a1 s) = a2 then (Some s) else None
+let update_var_subst x v s =
+  if not (is_mapped x s)
+  then Some (map_extend x v s)
+  else if (map_exn x s) = v then (Some s) else None
 
 let compute_var_subst p1 p2 =
   let rec compute_subst' p1 p2 s = match p1, p2 with
     | [], [] -> Some s
     | Instruction.PUSH (Const a1) :: t1, Instruction.PUSH (Const a2) :: t2 ->
-      Option.(update_var_subst a1 a2 s >>= compute_subst' t1 t2)
+      Option.(update_var_subst (Const a1) (Const a2) s >>= compute_subst' t1 t2)
     | i1 :: t1, i2 :: t2 when i1 = i2 -> compute_subst' t1 t2 s
     | _ -> None
   in compute_subst' p1 p2 []
--- test/test_subst.ml
+++ test/test_subst.ml
@@ -6,20 +6,24 @@ open Stackarg
 open Instruction
 open Subst
 
+let x = (Const "x")
+let y = (Const "y")
+let z = (Const "z")
+
 let suite = "suite" >::: [
 
     (* is_mapped *)
 
     "Variable is mapped in substitution">::(fun _ ->
-        assert_equal true (is_mapped "x" [("x", "y")])
+        assert_equal true (is_mapped x [(x, y)])
       );
 
     "Variable is not mapped in empty substitution">::(fun _ ->
-        assert_equal false (is_mapped "x" [])
+        assert_equal false (is_mapped x [])
       );
 
     "Variable is not mapped in substitution">::(fun _ ->
-        assert_equal false (is_mapped "x" [("z", "y")])
+        assert_equal false (is_mapped x [(z, y)])
       );
 
     (* map_exn *)
@@ -27,98 +31,98 @@ let suite = "suite" >::: [
     "Map when exists in substitution">::(fun _ ->
         assert_equal
           ~cmp:[%eq: constarg] ~printer:[%show: constarg]
-          "y" (map_exn "x" [("x", "y")])
+          "y" (map_exn x [(x, "y")])
       );
 
     (* map_extend *)
 
     "Extend empty substitution">::(fun _ ->
         assert_equal
-          ~cmp:[%eq: var_subst] ~printer:[%show: var_subst]
-          [("x", "y")] (map_extend "x" "y" [])
+          ~cmp:[%eq: Subst.tt] ~printer:[%show: Subst.t]
+          [(x, y)] (map_extend x y [])
       );
 
     "Extend substitution">::(fun _ ->
         assert_equal
-          ~cmp:[%eq: var_subst] ~printer:[%show: var_subst]
-          [("x", "y"); ("z", "v")] (map_extend "x" "y" [("z", "v")])
+          ~cmp:[%eq: Subst.t] ~printer:[%show: Subst.t]
+          [(x, y); (z, Const "v")] (map_extend x y [(z, Const "v")])
       );
 
     "Element is mapped in extended substitution">::(fun _ ->
-        let s = map_extend "x" "y" [] in
+        let s = map_extend x "y" [] in
         assert_equal
-          true (is_mapped "x" s)
+          true (is_mapped x s)
       );
 
-    (* update_var_subst *)
+    (* update_Subst.t *)
 
     "Update substitution where same mapping exists">::(fun _ ->
-        let s = [("x", "y")] in
+        let s = [(x, y)] in
         assert_equal
-          ~cmp:[%eq: var_subst option] ~printer:[%show: var_subst option]
-          (Some s) (update_var_subst "x" "y" s)
+          ~cmp:[%eq: Subst.t option] ~printer:[%show: Subst.t option]
+          (Some s) (update_var_subst x y s)
       );
 
     "Update substitution where different mapping exists">::(fun _ ->
-        let s = [("x", "y")] in
-        let s' = map_extend "z" "v" s in
+        let s = [(x, y)] in
+        let s' = map_extend z (Const "v") s in
         assert_equal
-          ~cmp:[%eq: var_subst option] ~printer:[%show: var_subst option]
-          (Some s') (update_var_subst "z" "v" s)
+          ~cmp:[%eq: Subst.t option] ~printer:[%show: Subst.t option]
+          (Some s') (update_var_subst z (Const "v") s)
       );
 
     "Fail when different mapping is inserted">::(fun _ ->
-        let s = [("x", "y")] in
+        let s = [(x, y)] in
         assert_equal
-          ~cmp:[%eq: var_subst option] ~printer:[%show: var_subst option]
-          None (update_var_subst "x" "v" s)
+          ~cmp:[%eq: Subst.t option] ~printer:[%show: Subst.t option]
+          None (update_var_subst x (Const "v") s)
       );
 
     "Fail when two different instructions are at same position">::(fun _ ->
         assert_equal
-          ~cmp:[%eq: var_subst option] ~printer:[%show: var_subst option]
+          ~cmp:[%eq: Subst.t option] ~printer:[%show: Subst.t option]
           None (compute_var_subst [ADD] [MUL])
       );
 
     "Succeed on equal programs without PUSH">::(fun _ ->
         assert_equal
-          ~cmp:[%eq: var_subst option] ~printer:[%show: var_subst option]
+          ~cmp:[%eq: Subst.t option] ~printer:[%show: Subst.t option]
           (Some []) (compute_var_subst [ADD; SUB] [ADD; SUB])
       );
 
     "Fail on programs with different size">::(fun _ ->
         assert_equal
-          ~cmp:[%eq: var_subst option] ~printer:[%show: var_subst option]
+          ~cmp:[%eq: Subst.t option] ~printer:[%show: Subst.t option]
           None (compute_var_subst [ADD; SUB] [ADD;])
       );
 
     "Succeed for PUSH with one argument">::(fun _ ->
         assert_equal
-          ~cmp:[%eq: var_subst option] ~printer:[%show: var_subst option]
-          (Some [("x", "y")]) (compute_var_subst [PUSH (Const "x")] [PUSH (Const "y");])
+          ~cmp:[%eq: Subst.t option] ~printer:[%show: Subst.t option]
+          (Some [(x, y)]) (compute_var_subst [PUSH x] [PUSH y;])
       );
 
     "Succeed for two PUSH with same arguments">::(fun _ ->
-        let l1 = [PUSH (Const "x"); PUSH (Const "x")] in
-        let l2 = [PUSH (Const "y"); PUSH (Const "y")] in
+        let l1 = [PUSH x; PUSH (Const "x")] in
+        let l2 = [PUSH y; PUSH (Const "y")] in
         assert_equal
-          ~cmp:[%eq: var_subst option] ~printer:[%show: var_subst option]
-          (Some [("x", "y")]) (compute_var_subst l1 l2)
+          ~cmp:[%eq: Subst.t option] ~printer:[%show: Subst.t option]
+          (Some [(x, y)]) (compute_var_subst l1 l2)
       );
 
     "Succeed for two PUSH mapping arguments to same argument">::(fun _ ->
-        let l1 = [PUSH (Const "x"); PUSH (Const "y")] in
-        let l2 = [PUSH (Const "z"); PUSH (Const "z")] in
+        let l1 = [PUSH x; PUSH y] in
+        let l2 = [PUSH z; PUSH z] in
         assert_equal
-          ~cmp:[%eq: var_subst option] ~printer:[%show: var_subst option]
-          (Some [("x", "z"); ("y", "z")]) (compute_var_subst l1 l2)
+          ~cmp:[%eq: Subst.t option] ~printer:[%show: Subst.t option]
+          (Some [(x, z); (y, z)]) (compute_var_subst l1 l2)
       );
 
     "Fail when two argument would need to map to different terms">::(fun _ ->
-        let l1 = [PUSH (Const "x"); PUSH (Const "x")] in
-        let l2 = [PUSH (Const "y"); PUSH (Const "z")] in
+        let l1 = [PUSH x; PUSH x] in
+        let l2 = [PUSH y; PUSH z] in
         assert_equal
-          ~cmp:[%eq: var_subst option] ~printer:[%show: var_subst option]
+          ~cmp:[%eq: Subst.t option] ~printer:[%show: Subst.t option]
           None (compute_var_subst l1 l2)
       );
  ]
