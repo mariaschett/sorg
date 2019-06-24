43808602db897a269fa130cf0e8a5194fd64fe53
Julian Nagele <mail@jnagele.net>
2019-06-24 14:44:56 +0100

    Introduce module and type for rewrite system

---
+++ lib/rewrite_system.ml
@@ -0,0 +1,22 @@
+open Core
+
+type t = Rule.t list
+
+let pp fmt rs =
+  Format.fprintf fmt "@[<v>";
+  List.iter rs ~f:(Format.fprintf fmt "%a@," Rule.pp);
+  Format.fprintf fmt "@]"
+
+let show rs = pp Format.str_formatter rs |> Format.flush_str_formatter
+
+let consts rs = List.stable_dedup @@ List.concat_map rs ~f:Rule.consts
+
+let pp_tpdb fmt ?(var="P") rs =
+  Format.fprintf fmt "@[<v>(VAR@[<hov>";
+  List.iter (consts rs) ~f:(fun v -> Format.fprintf fmt "@ %s" v);
+  Format.fprintf fmt "@])@,(RULES@,@[<v>";
+  List.iter rs ~f:(fun r -> Format.fprintf fmt "  %a@," (Rule.pp_tpdb ~var:var) r);
+  Format.fprintf fmt "@])@]"
+
+let show_tpdb ?(var="P") rs =
+  pp_tpdb Format.str_formatter ~var:var rs |> Format.flush_str_formatter
