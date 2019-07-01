cf944fb5315b31612ca283ffa193cb56f465494b
Maria A Schett <mail@maria-a-schett.net>
2019-07-01 12:28:51 +0100

    Add functionality to compute all contexts
    Currently wrong, tests fail

--- lib/ctxt.ml
+++ lib/ctxt.ml
@@ -1,6 +1,35 @@
 open Core
 open Ebso
+open Instruction
 
 type t = Program.t * Program.t [@@deriving show { with_path = false }, sexp, equal]
 
+let compare (pre1, post1) (pre2, post2) =
+  let compare_instr i1 i2 = match (i1, i2) with
+    | PUSH x, PUSH y -> Stackarg.compare x y
+    | _, _ -> Instruction.compare i1 i2
+  in
+  List.compare compare_instr (pre1 @ post1) (pre2 @ post2)
+
 let apply (pre, post) s = pre @ s @ post
+
+let ext_prefix cs i = List.map cs ~f:(fun (pre, post) -> (i::pre, post))
+
+let rec postfix s t = match (s, t) with
+  | [], _ -> Some t
+  | i1 :: s', i2:: t' when i1 = i2 -> postfix s' t'
+  | _, _ -> None
+
+let add_postfix cs t = List.map cs ~f:(fun (pre, post) -> (pre, post @ t))
+
+let rec all_ctxts s t = match (s, t) with
+  | i1 :: _, i2 :: t' ->
+    let cs = ext_prefix (all_ctxts s t') i2 in
+    if i1 = i2 then (match postfix s t with
+        | Some post -> add_postfix cs post
+        | None -> cs
+      )
+      else cs
+  | [], [] -> [([], [])]
+  | [], _  -> [([], t); (t, [])]
+  | _, []  -> []
--- test/test_ctxt.ml
+++ test/test_ctxt.ml
@@ -3,6 +3,14 @@ open Ebso
 open Ctxt
 open Instruction
 
+let comp_ctxts cs cs' =
+  let sort = List.sort Ctxt.compare in
+  [%eq: Ctxt.t list] (sort cs) (sort cs')
+
+let show_ctxts cs =
+  let sort = List.sort Ctxt.compare in
+  [%show: Ctxt.t list] (sort cs)
+
 let suite = "suite" >::: [
 
     (* apply *)
@@ -41,6 +49,106 @@ let suite = "suite" >::: [
           ~cmp:[%eq: Program.t]
           ~printer:[%show: Program.t]
           (pre @ s @ post) (apply (pre, post) s)
+      );
+
+    (* all_ctxts *)
+
+    "Context of program itself is empty" >:: (fun _ ->
+        let s = [POP] in
+        assert_equal
+          ~cmp:comp_ctxts
+          ~printer:show_ctxts
+          [([],[])] (all_ctxts s s)
+      );
+
+    "The empty program contains no non-empty program" >:: (fun _ ->
+        let s = [POP] in
+        let t = [] in
+        assert_equal
+          ~cmp:comp_ctxts
+          ~printer:show_ctxts
+          [] (all_ctxts s t)
+      );
+
+    "Every program contains the empty program" >:: (fun _ ->
+        let s = [] in
+        let t = [POP] in
+        assert_equal
+          ~cmp:comp_ctxts
+          ~printer:show_ctxts
+          [([], t); (t, [])] (all_ctxts s t)
+      );
+
+    "Context is only prefix" >:: (fun _ ->
+        let s = [POP] in
+        let t = [ADD; POP] in
+        assert_equal
+          ~cmp:comp_ctxts
+          ~printer:show_ctxts
+          [([ADD], []);] (all_ctxts s t)
+      );
+
+    "Context is only postfix" >:: (fun _ ->
+        let s = [POP] in
+        let t = [POP; ADD] in
+        assert_equal
+          ~cmp:comp_ctxts
+          ~printer:show_ctxts
+          [([], [ADD]);] (all_ctxts s t)
+      );
+
+    "Context is pre- and postfix" >:: (fun _ ->
+        let s = [POP] in
+        let t = [ADD; POP; ADD] in
+        assert_equal
+          ~cmp:comp_ctxts
+          ~printer:show_ctxts
+          [([ADD], [ADD]);] (all_ctxts s t)
+      );
+
+    "Program is contained via two different contexts" >:: (fun _ ->
+        let s = [POP] in
+        let t = [POP; POP] in
+        assert_equal
+          ~cmp:comp_ctxts
+          ~printer:show_ctxts
+          [([], [POP]); ([POP], [])] (all_ctxts s t)
+      );
+
+    "Program is contained via three different contexts" >:: (fun _ ->
+        let s = [POP] in
+        let t = [POP; POP; POP] in
+        assert_equal
+          ~cmp:comp_ctxts
+          ~printer:show_ctxts
+          [([], [POP;POP]); ([POP; POP], []); ([POP], [POP])] (all_ctxts s t)
+      );
+
+    "Programs with different immediate arguments (although instances)" >:: (fun _ ->
+        let s = [PUSH (Const "x")] in
+        let t = [PUSH (Const "y")] in
+        assert_equal
+          ~cmp:comp_ctxts
+          ~printer:show_ctxts
+          [] (all_ctxts s t)
+      );
+
+    "Programs with same immediate arguments" >:: (fun _ ->
+        let s = [PUSH (Const "x")] in
+        let t = [PUSH (Const "x")] in
+        assert_equal
+          ~cmp:comp_ctxts
+          ~printer:show_ctxts
+          [([], [])] (all_ctxts s t)
+      );
+
+    "A different program has no context" >:: (fun _ ->
+        let s = [PUSH (Const "x")] in
+        let t = [POP] in
+        assert_equal
+          ~cmp:comp_ctxts
+          ~printer:show_ctxts
+          [] (all_ctxts s t)
       );
   ]
 
