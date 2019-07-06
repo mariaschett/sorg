d4c1dc3e988fcce129d01c7309b7480885797a15
Maria A Schett <mail@maria-a-schett.net>
2019-07-06 17:56:03 +0100

    Move vars and size together

--- lib/rewrite_system.ml
+++ lib/rewrite_system.ml
@@ -18,6 +18,8 @@ let contains_rule rs r = List.mem rs r ~equal:[%eq: Rule.t] (* identifies alpha 
 
 let size = List.length
 
+let vars rs = List.stable_dedup @@ List.concat_map rs ~f:Rule.vars
+
 let rec insert_max_general r rs =
   let is_instance_rule r r' = Subst.is_instance (r.lhs @ r.rhs) (r'.lhs @ r'.rhs) in
   match rs with
@@ -32,8 +34,6 @@ let insert_non_dup_rules rs' rs =
     if contains_rule rs r then (rs, r :: dups) else (r :: rs, dups)
   in
   List.fold rs' ~init:(rs, []) ~f:insert
-
-let vars rs = List.stable_dedup @@ List.concat_map rs ~f:Rule.vars
 
 (* print in tpdb format *)
 
