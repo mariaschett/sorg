35343056c57ee90df20abf23ab6892d43990fcc7
Maria A Schett <mail@maria-a-schett.net>
2019-07-06 17:56:54 +0100

    Add line breaks

--- lib/rewrite_system.ml
+++ lib/rewrite_system.ml
@@ -14,14 +14,17 @@ let pp fmt rs =
 
 let show rs = pp Format.str_formatter rs |> Format.flush_str_formatter
 
-let contains_rule rs r = List.mem rs r ~equal:[%eq: Rule.t] (* identifies alpha equvivalent rules *)
+(* identifies alpha equvivalent rules *)
+let contains_rule rs r = List.mem rs r ~equal:[%eq: Rule.t]
 
 let size = List.length
 
 let vars rs = List.stable_dedup @@ List.concat_map rs ~f:Rule.vars
 
 let rec insert_max_general r rs =
-  let is_instance_rule r r' = Subst.is_instance (r.lhs @ r.rhs) (r'.lhs @ r'.rhs) in
+  let is_instance_rule r r' =
+    Subst.is_instance (r.lhs @ r.rhs) (r'.lhs @ r'.rhs)
+  in
   match rs with
   | [] -> [r]
   | r' :: rs' ->
